53c58880c0afac99c9584c73e4d385f0b7f34ce1
Andrew Loucky <andrew.loucky@gmail.com>
2016-05-31 21:31:32 -0400

    a step away from being fed a string and returning morse

--- morse_trainer.py
+++ morse_trainer.py
@@ -1,8 +1,10 @@
+import sys
+
 CODE = {
     'a': '.-',   'b': '-...', 'c': '-.-.', 'd': '-..',  'e': '.',
     'f': '..-.', 'g': '--.',  'h': '....', 'i': '..',   'j': '.---',
     'k': '-.-',  'l': '.-..', 'm': '--',   'n': '-.',   'o': '---',
-    'p': '.--.', 'q': '--.-', 'r': '.-.',  's': '---',  't': '.',
+    'p': '.--.', 'q': '--.-', 'r': '.-.',  's': '...',  't': '-',
     'u': '..-',  'v': '...-', 'w': '.--',  'x': '-..-', 'y': '-.--',
     'z': '--..',
 
@@ -10,17 +12,15 @@ CODE = {
     '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.'
     }
 
-print CODE['a']
+def verify_message(msg):
+    keys = CODE.keys()
+    for char in msg:
+        if char not in keys and char != ' ':
+            sys.exit('Error, invalid character: ' + char)
 
-def valid_morse(char):
-    if char not in CODE and char != ' ':
-        sys.exit('Error, invalid character: ' + char)
-    else:
-        return True
-
-def print_morse(string):
-    for char in string:
-        if valid_morse(char):
-            print CODE[char]
+def print_morse(msg):
+    verify_message(msg)
+    for char in msg:
+        print CODE[char]
 
 print_morse('test')
